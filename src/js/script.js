89b50ababb018815486039d6af033219a0230494
Eric Pham <ericpham380@gmail.com>
2018-07-30 16:37:49 -0700

    Added src folder and files

---
+++ src/js/script.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  var main = document.getElementById('main');
+  var button = document.createElement('button');
+  var para = document.getElementById('paragraph');
+
+  button.innerHTML = "Display status";
+
+  button.addEventListener("click", function() {
+    if(para.className === 'hidden') {
+      para.classList.remove('hidden');
+      para.classList.add('d-block');
+    } else if(para.className === 'd-block') {
+      para.classList.add('hidden');
+      para.classList.remove('d-block');
+    }
+  });
+
+  main.appendChild(button);
+});

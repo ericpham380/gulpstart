ed5c43351460a8f0aaea9cc75f44618b23ee8244
Eric Pham <ericpham380@gmail.com>
2018-07-31 09:52:40 -0700

    Created task to inject css and js files into index.html

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp');
-
+var inject = require('gulp-inject');
 
 var paths = {
   src: 'src/**/*',
@@ -22,19 +22,38 @@ gulp.task('hello', function() {
   console.log('Hello World!');
 });
 
+gulp.task('default', function() {
+  console.log('Default task from gulpfile.js...');
+})
+
+/* Setup the HTML task to copy all HTML files from src to tmp directory where you'll be running the web server */
 gulp.task('html', function() {
   return gulp.src(paths.srcHTML)
     .pipe(gulp.dest(paths.tmp));
 });
 
+/* Setup the CSS task */
 gulp.task('css', function() {
   return gulp.src(paths.srcCSS)
     .pipe(gulp.dest(paths.tmp));
 });
 
+/* Setup the JavaScript task */
 gulp.task('js', function() {
   return gulp.src(paths.srcJS)
     .pipe(gulp.dest(paths.tmp));
 });
 
+/* Combine all tasks into one task */
 gulp.task('copy', ['html', 'css', 'js']);
+
+/* Inject CSS and JavaScript into the index.html */
+gulp.task('inject', ['copy'], function() {
+  var css = gulp.src(paths.tmpCSS);
+  var js = gulp.src(paths.tmpJS);
+  return gulp.src(paths.tmpIndex)
+    .pipe(inject(css, { relative: true } ))
+    .pipe(inject(js, { relative: true } ))
+    .pipe(gulp.dest(paths.tmp));
+});
+

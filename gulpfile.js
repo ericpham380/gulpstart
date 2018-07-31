4cac3d1cf1daa05b976cd1deb9369a2ec568e511
Eric Pham <ericpham380@gmail.com>
2018-07-30 23:14:31 -0700

    Implemented copy task from src to tmp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,40 @@
 var gulp = require('gulp');
 
+
+var paths = {
+  src: 'src/**/*',
+  srcHTML: 'src/**/*.html',
+  srcCSS: 'src/**/*.css',
+  srcJS: 'src/**/*.js',
+
+  tmp: 'tmp',
+  tmpIndex: 'tmp/index.html',
+  tmpCSS: 'tmp/**/*.css',
+  tmpJS: 'tmp/**/*.js',
+
+  dist: 'dist',
+  distIndex: 'dist/index.html',
+  distCSS: 'tmp/**/*.css',
+  distJS: 'dist/**/*.js'
+};
+
 gulp.task('hello', function() {
   console.log('Hello World!');
 });
+
+gulp.task('html', function() {
+  return gulp.src(paths.srcHTML)
+    .pipe(gulp.dest(paths.tmp));
+});
+
+gulp.task('css', function() {
+  return gulp.src(paths.srcCSS)
+    .pipe(gulp.dest(paths.tmp));
+});
+
+gulp.task('js', function() {
+  return gulp.src(paths.srcJS)
+    .pipe(gulp.dest(paths.tmp));
+});
+
+gulp.task('copy', ['html', 'css', 'js']);
